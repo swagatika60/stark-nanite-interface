03758649b0780ae7637e1ca024b1e4396c4c12de
Swagatika Beura <swagatikab25@gmail.com>
2025-12-12 07:29:22 +0530

    added a new file

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+// App.jsx
+import StarkLabGestureCommand from "./StarkLabGestureCommand";
+
+function App() {
+  return (
+    <div style={{ margin: 0, padding: 0, backgroundColor: "#000510" }}>
+      <StarkLabGestureCommand />
+    </div>
+  );
+}
+
+export default App;
